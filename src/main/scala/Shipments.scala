54fedd9b6a571c25c9ca5367414f35bd04f15763
Dan Clark <dan.clark@quanttus.com>
2015-01-20 08:28:06 -0500

    add Shipments capability

---
+++ src/main/scala/Shipments.scala
@@ -0,0 +1,12 @@
+import scala.slick.driver.PostgresDriver.simple._
+
+case class Shipment(snum: String, pnum: String, qty: Int)
+
+class Shipments(tag: Tag) extends Table[Shipment](tag, "shipments") {
+  def snum = column[String]("snum")
+  def pnum = column[String]("pnum")
+  def qty = column[Int]("qty")
+  def * = (snum, pnum, qty) <> (Shipment.tupled, Shipment.unapply _)
+  def supplier = foreignKey("SUP_FK", snum, suppliers)(_.snum)
+}
+val shipments = TableQuery[Shipments]
